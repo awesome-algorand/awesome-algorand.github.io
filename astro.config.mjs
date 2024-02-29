b3ef77f8ff98ca2e501ac001ca6e1bc35aaa613a
Michael Feher <michael.feher@algorand.foundation>
2024-02-29 16:50:09 -0600

    🎉 create an @astrojs/starlight project

---
+++ astro.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Awesome Algorand',
+			social: {
+				github: 'https://github.com/awesome-algorand',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
